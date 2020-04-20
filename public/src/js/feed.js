340c1439ee6b776da18c2ee12fbfc7b13bc44cb2
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-04-20 22:00:01 +0100

    implementation of caching then network, caching only and caching with network fallback

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -30,7 +30,26 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-function createCard() {
+// currently not in use
+const saveForOffline = (e) => {
+  console.log('clicked');
+  if('caches' in window){
+    caches.open('user-v1').then(cache => {
+      cache.add('/src/images/sf-boat.jpg');
+      cache.add('https://httpbin.org/get');
+    })
+  }
+}
+
+//clearing of duplicate createCard
+const removeCard = (msg) => {
+  while(sharedMomentsArea.hasChildNodes()){
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+    console.log(msg)
+  }
+}
+
+function createCard(msg) {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
   var cardTitle = document.createElement('div');
@@ -48,15 +67,43 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  // const saveButton = document.createElement('button');
+  // saveButton.textContent = 'save';
+  // saveButton.addEventListener('click', saveForOffline);
+  // cardSupportingText.appendChild(saveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
+  console.log(msg)
 }
+
+const url = 'https://httpbin.org/get';
+let networkDataReceived = false;
 
 fetch('https://httpbin.org/get')
   .then(function(res) {
     return res.json();
   })
   .then(function(data) {
-    createCard();
+    console.log('the networks have it: ', data)
+    networkDataReceived = true;
+    console.log('from the network, networkdatareceived: ', networkDataReceived);
+    removeCard('the network is doing the replacing')
+    createCard('this is the network');
   });
+
+if('caches' in window){
+  caches.match(url).then(response => {
+    if(response){ 
+      return response.json();
+    }
+  }).then(data => {
+    if(!networkDataReceived){
+      console.log('from the cache, networkdatareceived: ', networkDataReceived);
+      console.log('coming from the caches now we here: ', data);
+      removeCard('the cache is doing the replacing');
+      createCard('this is the cache');
+    }
+    
+  })
+}
--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,30 @@
-const STATIC_CACHE_NAME = 'static-v1';
-const STATIC_DYNAMIC_NAME = 'dynamic-v1';
+const STATIC_CACHE_NAME = 'static-v5';
+const DYNAMIC_CACHE_NAME = 'dynamic-v5';
+const STATIC_ASSETS = [
+                    '/', 
+                    '/index.html', 
+                    '/offline.html',
+                    '/src/js/app.js', 
+                    '/src/js/feed.js', 
+                    '/src/js/fetch.js', 
+                    '/src/js/material.min.js',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                ]
+
+const staticOnly = (arr, req) => {
+    arr.forEach(asset => {
+        if(req.indexOf(asset) > -1){
+            return true;
+        }
+    })
+}
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...');
   event.waitUntil(caches.open(STATIC_CACHE_NAME).then(cache => {
-      cache.addAll(['/', 
-                    '/index.html', 
-                    '/src/js/app.js', 
-                    '/src/js/feed.js', 
-                    '/src/js/fetch.js', 
-                    '/src/js/material.min.js'
-                ])
+      cache.addAll(STATIC_ASSETS)
   }).catch(err => {
       console.log(err);
   }))
@@ -20,7 +34,7 @@ self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....');
   event.waitUntil(caches.keys().then(keyList => {
       return Promise.all(keyList.map(key => {
-          if(key !== STATIC_CACHE_NAME && key !== STATIC_DYNAMIC_NAME){
+          if(key !== STATIC_CACHE_NAME && key !== DYNAMIC_CACHE_NAME){
               console.log('removing old cache - ', key);
               caches.delete(key);
           }
@@ -30,15 +44,36 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(response => {
-      if(response) {
-          return response;
-      }
-      return fetch(event.request).then(res => {
-          return caches.open(STATIC_DYNAMIC_NAME).then(cache => {
-              cache.put(event.request.url, res.clone());
-              return res;
-          })
-      });
-  }))
+    const url = 'https://httpbin.org/get';
+    if(event.request.url.indexOf(url) > -1){
+        event.respondWith(caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+            return fetch(event.request).then(res => {
+                cache.put(event.request.url, res.clone());
+                return res;
+            }) 
+        }))
+    }else if(staticOnly(STATIC_ASSETS, event.request.url)){
+        event.respondWith(
+            caches.match(event.request)
+        )
+    }else{
+        event.respondWith(caches.match(event.request).then(response => {
+            if(response){
+                return response;
+            }
+            return fetch(event.request).then(response => {
+                return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                })
+            }).catch(err => {
+                return caches.open(STATIC_CACHE_NAME).then(cache => {
+                    if(event.request.url.indexOf('/help') > -1){
+                        return cache.match('/offline.html');
+                    }
+                })
+            })
+        }))
+    }
+  
 });
